f780816d9ec83a7d1dc2424a9d21c426205b82d4
antaresride <antares@ride.com>
2026-03-07 14:45:30 -0300

    Testing the library integration and CI/CD execution

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
-    println!("Genpay Client Running");
+    println!(
+        "Testing the library integration and CI/CD execution, the lexer returned is: {}",
+        lexer::add(1, 1)
+    );
 }
-

f87bdfddf64f436b5a78e093357942530ecaea3d
antaresride <antares@ride.com>
2026-03-10 16:20:45 -0300

    Configured the perfomance tests for linker, codegen using mimalloc

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+#[cfg(feature = "use_mimalloc")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 fn main() {
     #[cfg(feature = "use_cranelift")]
     println!("{}", codegen::identify_architecture_aarch64());
+    println!("GenPayLabs!");
 }

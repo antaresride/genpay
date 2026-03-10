547296a9a0cf8154675282751b3c140afbaa7c74
antaresride <antares@ride.com>
2026-03-10 14:07:05 +0000

    Added optional gating features for use Minimalloc Allocator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#[cfg(feature = "use_mimalloc")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 fn main() {
     println!("{}", codegen::identify_architecture_aarch64());
 }
